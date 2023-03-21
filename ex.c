72d9e743db7bceebc32b6da550f565d579aac48b
FaelGP <128199511+FaelGP@users.noreply.github.com>
2023-03-21 10:45:08 -0300

    Update ex.c

--- ex.c
+++ ex.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
-int main()
-{
-    printf("Ola");
-    return 0;
+#include<stdio.h>
+
+main(){
+    
+    float produto, lucro, custo, venda, despesa, frete;
+    
+		printf("Informe o valor do produto:\n");
+    	scanf("%f",&produto);
+        
+		printf("Informe o valor do frete:\n");
+    	scanf("%f",&frete);
+        
+		printf("Informe o quanto foi gasto com despesas:\n");
+    	scanf("%f",&despesa);
+    	
+		custo = produto + frete + despesa;
+        
+		printf("Qual o valor da venda :");
+    	scanf("%f",&venda);
+    
+		lucro = ((venda-custo)/custo*100);
+    
+		printf("O lucro neste produto e de :%.2f%%", lucro);
+		
+		return 0;
 }
